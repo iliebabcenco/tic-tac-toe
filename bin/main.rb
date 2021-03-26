01683e3559703334451be3b90b6e48313dd3bbb3
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-03-26 15:02:38 +0200

    finish main_process method, add main_process_logic as a part of future logic, add some empty lines for better look

--- bin/main.rb
+++ bin/main.rb
@@ -1,94 +1,91 @@
 #!/usr/bin/env ruby
-class Game 
-    attr_accessor :player1, :player2, :players
+class Game
+  attr_accessor :player1, :player2, :players
 
-    # @@var1 = 1
-    # @@var2 = 2
-    # @@var3 = 3
-    # @@var4 = 4
-    # @@var5 = 5
-    # @@var6 = 6
-    # @@var7 = 7
-    # @@var8 = 8
-    # @@var9 = 9
+  def initialize()
+    @player1 = nil
+    @player2 = nil
+    @players = {}
+    @game_over = false
+    @counter = 0
+    @choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @winner = 'TIE'
+  end
 
-    
-    def initialize()
-        @player1=nil
-        @player2=nil
-        @players = {}
-        @game_over = false
-        @counter = 0
-        @choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  def draw_board
+    puts '+---+---+---+'
+    puts "| #{@choices[0]} | #{@choices[1]} | #{@choices[2]} |"
+    puts '+---+---+---+'
+    puts "| #{@choices[3]} | #{@choices[4]} | #{@choices[5]} |"
+    puts '+---+---+---+'
+    puts "| #{@choices[6]} | #{@choices[7]} | #{@choices[8]} |"
+    puts '+---+---+---+'
+  end
+
+  def initialize_game
+    puts
+    puts 'Welcome to the game'
+    puts
+    puts 'Enter Player 1 name: '
+    @player1 = gets.chomp
+    @players['X'] = @player1
+    puts 'Enter player 2 name: '
+    @player2 = gets.chomp
+    @players['O'] = @player2
+    sleep(1)
+    puts
+    puts "#{@player1} will be X, and #{@player2} will be O."
+    sleep(1)
+    puts
+    puts "Let's start!"
+    puts
+    sleep(1)
+  end
+
+  def main_process
+    @players.values.each do |player|
+      draw_board
+      puts
+      puts "It's #{player}'s turn"
+      puts 'Please select an available cell from the board: '
+      # there should be full logic of main process
+      main_process_logic(player)
+      # end of logic
     end
+  end
 
-    def draw_board
-        puts '+---+---+---+'
-        puts "| #{@choices[0]} | #{@choices[1]} | #{@choices[2]} |"
-        puts '+---+---+---+'
-        puts "| #{@choices[3]} | #{@choices[4]} | #{@choices[5]} |"
-        puts '+---+---+---+'
-        puts "| #{@choices[6]} | #{@choices[7]} | #{@choices[8]} |"
-        puts '+---+---+---+'
-    end
-
-    def initialize_game 
-        puts 'Welcome to the game'
-        puts 'Enter Player 1 name: '
-        @player1 = gets.chomp
-        @players["X"] = @player1
-        puts 'Enter player 2 name: '
-        @player2 = gets.chomp
-        @players["O"] = @player2
-        sleep(1)
-        puts "#{@player1} will be X, and #{@player2} will be O."
-        sleep(1)
-        puts "Let's start!"
-        sleep(1)
-    end
-
-    def make_choice
-        
-            for player in @players.values # 2
-                if @counter < 9
-                    draw_board()
-                    puts "It's #{player} turn"
-                    puts "Please select an available cell from the board: "
-                    while (true)
-                        choice = gets.chomp.to_i
-                        if (@choices.include?(choice))
-                            @counter += 1
-                            @choices[choice-1] = @players.key(player)
-                            break
-                        else
-                            puts "Invalid input, #{player} should try again"   
-                        end
-                    end
-                else
-                    @game_over = true
-                    return
-                end
-            end
-        
-    end
-
-
-    def start_game
-        initialize_game
-        until (@game_over)
-            
-            make_choice
-            puts 'Game over'
-            
+  # this method is just a part of future logic
+  def main_process_logic(player)
+    loop do
+      if @counter < 9
+        choice = gets.chomp.to_i
+        if @choices.include?(choice)
+          @counter += 1
+          @choices[choice - 1] = @players.key(player)
+          break
+        else
+          puts "Invalid input, #{player} should try again (input should be a number form 1 to 9)"
         end
+      else
+        @winner = @player1
+        @game_over = true
+        return 0
+      end
     end
+  end
 
-
-
-
-
+  def start_game
+    initialize_game
+    main_process until @game_over
+    if @winner == 'TIE'
+      puts "It's TIE"
+    else
+      puts "#{@winner} is Winner!"
+    end
+    puts
+    puts 'Game over'
+  end
 end
 
 game = Game.new
 game.start_game
-
