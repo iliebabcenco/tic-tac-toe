f6c5a095d70cb0b515953c36630ad31e0637af95
Marko Zecevic <marczecevic@gmail.com>
2021-03-26 09:44:02 +0100

    Add draw_board and initialize_game methods

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,37 @@
 #!/usr/bin/env ruby
-puts "Hello world!"
+class Game 
+
+    def initialize()
+
+    end
+
+    def draw_board
+        puts '+---+---+---+'
+        puts '| 1 | 2 | 3 |'
+        puts '+---+---+---+'
+        puts '| 4 | 5 | 6 |'
+        puts '+---+---+---+'
+        puts '| 7 | 8 | 9 |'
+        puts '+---+---+---+'
+
+    end
+
+    def initialize_game 
+        puts 'Welcome to the game'
+        puts 'Enter Player 1 name: '
+        player1 = gets.chomp
+        puts 'Enter player 2 name: '
+        player2 = gets.chomp
+    end
+
+    
+
+
+
+end
+
+    
+
+game1 = Game.new
+game1.initialize_game
+game1.draw_board
