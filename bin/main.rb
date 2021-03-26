d14fc02c9714eab650f1d0b3176fb3477918d0bc
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-03-26 12:00:19 +0200

    update make_choice method

--- bin/main.rb
+++ bin/main.rb
@@ -2,31 +2,33 @@
 class Game 
     attr_accessor :player1, :player2, :players
 
-    @@var1 = 1
-    @@var2 = 2
-    @@var3 = 3
-    @@var4 = 4
-    @@var5 = 5
-    @@var6 = 6
-    @@var7 = 7
-    @@var8 = 8
-    @@var9 = 9
+    # @@var1 = 1
+    # @@var2 = 2
+    # @@var3 = 3
+    # @@var4 = 4
+    # @@var5 = 5
+    # @@var6 = 6
+    # @@var7 = 7
+    # @@var8 = 8
+    # @@var9 = 9
+
     
     def initialize()
         @player1=nil
         @player2=nil
-        @players = []
+        @players = {}
         @game_over = false
         @counter = 0
+        @choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     end
 
     def draw_board
         puts '+---+---+---+'
-        puts "| #{@@var1} | #{@@var2} | #{@@var3} |"
+        puts "| #{@choices[0]} | #{@choices[1]} | #{@choices[2]} |"
         puts '+---+---+---+'
-        puts "| #{@@var4} | #{@@var5} | #{@@var6} |"
+        puts "| #{@choices[3]} | #{@choices[4]} | #{@choices[5]} |"
         puts '+---+---+---+'
-        puts "| #{@@var7} | #{@@var8} | #{@@var9} |"
+        puts "| #{@choices[6]} | #{@choices[7]} | #{@choices[8]} |"
         puts '+---+---+---+'
     end
 
@@ -34,10 +36,10 @@ class Game
         puts 'Welcome to the game'
         puts 'Enter Player 1 name: '
         @player1 = gets.chomp
-        @players.push(@player1)
+        @players["X"] = @player1
         puts 'Enter player 2 name: '
         @player2 = gets.chomp
-        @players.push(@player2)
+        @players["O"] = @player2
         sleep(1)
         puts "#{@player1} will be X, and #{@player2} will be O."
         sleep(1)
@@ -46,11 +48,19 @@ class Game
     end
 
     def make_choice
-        for player in @players
+        for player in @players.values
             draw_board()
             puts "It's #{player} turn"
             puts "Please select an available cell from the board: "
-            choice = gets.chomp
+            while (true)
+                choice = gets.chomp.to_i
+                if (@choices.include?(choice))
+                    @choices[choice-1] = @players.key(player)
+                    break
+                else
+                    puts "Invalid input, #{player} should try again"
+                end
+            end
         end
     end
 
